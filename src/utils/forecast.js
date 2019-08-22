e742e2e2b1fe9a2ef54124f3e0b53e37e8db312b
amir <amiryehu8@gmail.com>
2019-08-22 18:00:08 +0300

    Add high and low degrees

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to finde location.', undefined)           
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' desrees out. there is a ' + body.currently.precipProbability + '% chnce of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' desrees out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chnce of rain.')
         }
 
     })
